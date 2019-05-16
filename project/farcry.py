cea00a40ba68f8b18e64cdd0d6b6e0a2edffd895
dnhu <dinhquynhnhu14@gmail.com>
2019-05-16 16:16:25 +0700

    wp3-4-5

--- project/farcry.py
+++ project/farcry.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def read_log_file(log_file_pathname):
     """
     Waypoint 1: Read Game Session Log File
@@ -22,7 +23,7 @@ def read_log_file(log_file_pathname):
 
 def parse_log_start_time(log_data):
     """
-    Waypoint 2: Parse Far Cry Engine's Start Time
+    Waypoint 2 + 3: Parse Far Cry Engine's Start Time + Time Zone
     ---
     Parse this date and time information to determine later the timestamp of each frag.
     ---
@@ -32,15 +33,54 @@ def parse_log_start_time(log_data):
     """
 
     from datetime import datetime
+    from datetime import timedelta
+    from datetime import timezone
+    
+    # get time zone 
+    index_timezone = log_data.find('g_timezone') + 11
+    value_timezone = log_data[index_timezone: index_timezone+5].split('\n')[0][:-1]
+    g_timezone = timezone(timedelta(hours=int(value_timezone)))
 
+    # get local start time 
     first_line = log_data.split('\n')[0][15:]
     format_form = "%A, %B %d, %Y %H:%M:%S" # Friday, November 09, 2018 12:22:07
-    start_time = datetime.strptime(first_line, format_form)
+    start_time_local = datetime.strptime(first_line, format_form)
 
+    # add timezone into local start time
+    start_time = start_time_local.replace(tzinfo=g_timezone)
+   
     return start_time
    
 
+def parse_match_mode_and_map(log_data):
+    """
+    Waypoint 4: Parse Match Session's Mode and Map
+    ---
+    Finding key word 'Loading level' and retrive index of the word that indicates the mode and the map
+    ---
+    @param {str} log_data: content of log file
+    @return {tuple} (mode, map).
+
+    """
+    index = log_data.find('Loading level') + 21
+    mode_and_map = log_data[index: index+50].split(' ')
+    map = mode_and_map[0][:-1]
+    mode = mode_and_map[2]
+    
+    return (mode, map)
+
+
+def parse_frags(log_data):
+    from re import findall
+    
+    patterm = "<\d{2}:\d{2}>\s<\w*>\s\w*\skilled\s\w*\swith\s\w*"
+    frag = findall(patterm, log_data)
+    print(frag)
+
 if __name__ == "__main__":
-    log_data = read_log_file("../logs/log00.txt")
-    log_start_time = parse_log_start_time(log_data)
-    print(log_start_time.isoformat())
+    log_data = read_log_file("../logs/log09.txt")
+    wp2_3 = parse_log_start_time(log_data)
+    # print(wp2_3)
+    wp4 = parse_match_mode_and_map(log_data)
+    # print(wp4, type(wp4))
+    parse_frags(log_data)
